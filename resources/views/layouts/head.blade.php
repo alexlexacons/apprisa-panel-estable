bb0137d51d71a38aab27ff8dbce48b8964294022
alexlexacons <desarrollo-01@asicofi.com>
2023-12-17 18:13:47 -0600

    feat:
    Agregado de favicon.
    
    Co-Authored-By: alexlexacons <151779948+alexlexacons@users.noreply.github.com>

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,6 +6,7 @@
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="shortcut icon" href="{{ asset('img/logo.png') }}" type="image/x-icon">
 
     <title>Panel Administrador</title>
 </head>
